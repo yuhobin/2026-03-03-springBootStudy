729755b8bf5fa188746051ce341807807bc2276b
yuhobin <dptmzh12@gmail.com>
2026-09-10 09:49:21 +0900

    2026-08-24 websocket 알림 기능 실습

---
+++ SpringWebSocket_1/src/main/java/com/sist/web/SpringWebSocket1Application.java
@@ -0,0 +1,13 @@
+package com.sist.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSocket1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSocket1Application.class, args);
+	}
+
+}
---
+++ SpringWebSocket_2/src/main/java/com/sist/web/SpringWebSocket2Application.java
@@ -0,0 +1,13 @@
+package com.sist.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSocket2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSocket2Application.class, args);
+	}
+
+}
